9160d6de511005b2388407d10ba96b92197dd381
Gus Umbelino <gumbelin@gmail.com>
2020-10-04 22:23:09 -0500

    fetching data

--- components/Banner.js
+++ components/Banner.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text } from 'react-native';
 
 const Banner = ({title}) => (
-    <Text style={styles.bannerStyle}>{title}</Text>
+    <Text style={styles.bannerStyle}>{title || '[loading...]'}</Text>
   );
 
   const styles = StyleSheet.create({
