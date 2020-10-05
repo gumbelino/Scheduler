4a0fe674f1111c954b41b0f7599c3eb28ac2621f
Gus Umbelino <gumbelin@gmail.com>
2020-10-04 23:00:26 -0500

    filtering

--- App.js
+++ App.js
@@ -1,46 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, SafeAreaView} from 'react-native';
-import Banner from './components/Banner';
-import CourseList from './components/CourseList'
+import React, { useState, useEffect } from "react";
+import { StyleSheet, SafeAreaView } from "react-native";
+import Banner from "./components/Banner";
+import CourseList from "./components/CourseList";
 
 const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": [
+  title: "CS Courses for 2018-2019",
+  courses: [
     {
-      "id": "F101",
-      "title": "Computer Science: Concepts, Philosophy, and Connections",
-      "meets": "MWF 11:00-11:50"
+      id: "F101",
+      title: "Computer Science: Concepts, Philosophy, and Connections",
+      meets: "MWF 11:00-11:50",
     },
     {
-      "id": "F110",
-      "title": "Intro Programming for non-majors",
-      "meets": "MWF 10:00-10:50"
+      id: "F110",
+      title: "Intro Programming for non-majors",
+      meets: "MWF 10:00-10:50",
     },
     {
-      "id": "F111",
-      "title": "Fundamentals of Computer Programming I",
-      "meets": "MWF 13:00-13:50"
+      id: "F111",
+      title: "Fundamentals of Computer Programming I",
+      meets: "MWF 13:00-13:50",
     },
     {
-      "id": "F211",
-      "title": "Fundamentals of Computer Programming II",
-      "meets": "TuTh 12:30-13:50"
-    }
-  ]
+      id: "F211",
+      title: "Fundamentals of Computer Programming II",
+      meets: "TuTh 12:30-13:50",
+    },
+  ],
 };
 
 const App = () => {
-  const [schedule, setSchedule] = useState({ title: '', courses: [] });
+  const [schedule, setSchedule] = useState({ title: "", courses: [] });
 
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+  const url = "https://courses.cs.northwestern.edu/394/data/cs-courses.php";
 
   useEffect(() => {
-    const fetchSchedule =  async () => {
+    const fetchSchedule = async () => {
       const response = await fetch(url);
       if (!response.ok) throw response;
       const json = await response.json();
       setSchedule(json);
-    }
+    };
     fetchSchedule();
   }, []);
 
@@ -50,15 +50,15 @@ const App = () => {
       <CourseList courses={schedule.courses} />
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingTop: 20,
-  }
+  },
 });
 
 export default App;
--- components/CourseList.js
+++ components/CourseList.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import { StyleSheet, View, ScrollView } from 'react-native';
-import Course from './Course';
+import React, { useState } from "react";
+import { StyleSheet, View, ScrollView } from "react-native";
+import Course from "./Course";
+import TermSelector from "./TermSelector";
 
-const CourseList = ({courses}) => (
+const termMap = { F: "Fall", W: "Winter", S: "Spring" };
+const terms = Object.values(termMap);
+
+const getCourseTerm = (course) => termMap[course.id.charAt(0)];
+
+const CourseList = ({ courses }) => {
+  const [selectedTerm, setSelectedTerm] = useState("Fall");
+
+  const termCourses = courses.filter(
+    (course) => selectedTerm === getCourseTerm(course)
+  );
+
+  return (
     <ScrollView>
       <View style={styles.courseList}>
-        {courses.map(course => <Course key={course.id} course={course} />) }
+        <TermSelector
+          selectedTerm={selectedTerm}
+          setSelectedTerm={setSelectedTerm}
+        />
+        {termCourses.map((course) => (
+          <Course key={course.id} course={course} />
+        ))}
       </View>
     </ScrollView>
   );
+};
 
 const styles = StyleSheet.create({
-    courseList: {
-      flex: 1,
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-    }
-  });
+  courseList: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+});
 
-  export default CourseList;
+export default CourseList;
